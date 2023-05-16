cbf0f742f292feb2a24cc94bc6e232e02f305271
Sini Aaltonen <siniaaltonen@outlook.com>
2023-05-16 20:49:51 +0300

    Added rect state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,33 @@
 import Todo from '././components/Todo';
+import { useState } from 'react';
 
 function App() {
+  const [choreList, setchoreList]=useState([{name:'Yrttien kasvatusalustan rakentaminen'}, {name: 'Maalaus Verso maalilla'}, {name: 'Mökin maalaus'}]);
+  const [inputText, setinputText]=useState('');
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <h1>Salajärven mökin askareet:</h1>
-      <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '10px' }}>
-        <div style={{ marginRight: '10px' }}>
-          <Todo text='Yrttien kasvatusalustan rakentaminen' />
+      <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '10px', flexWrap: 'wrap', rowGap: '10px', columnGap: '10px'}}>
+       {
+        choreList.map(item => (
+          <div>
+          <Todo text={item.name} />
         </div>
-        <div style={{ marginBottom: '10px' }}>
-          <Todo text='Yrttilaatikon maalaus Tikkurilan Versolla' />
-        </div>
+        ))
+        }
       </div>
-      <div style={{ flexShrink: 0, marginBottom: '10px', marginRight: '10px' }}>
-        <Todo text='Kasvatusalustan maan muokkaus' />
-      </div>
+      <form>
+        <input type='text' onChange={event => setinputText(event.target.value)} value={inputText}/>
+        <button onClick={event => {
+          event.preventDefault()
+          if (inputText.length > 0){setchoreList([...choreList,{name:inputText}])}
+          setinputText('')
+          }
+          }>
+          Lisää
+        </button>
+      </form>
+      {inputText}
     </div>
   );
 }
