ee62e9b0cd98d6914390fdad1cfa5b8631a28020
Sini Aaltonen <siniaaltonen@outlook.com>
2023-05-16 14:41:09 +0300

    Components in right places

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,18 @@ import Todo from '././components/Todo';
 
 function App() {
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
       <h1>Salajärven mökin askareet:</h1>
-      <div style={{ marginBottom: '10px' }}>
-        <Todo text='Yrttien kasvatusalustan rakentaminen' />
+      <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '10px' }}>
+        <div style={{ marginRight: '10px' }}>
+          <Todo text='Yrttien kasvatusalustan rakentaminen' />
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <Todo text='Yrttilaatikon maalaus Tikkurilan Versolla' />
+        </div>
       </div>
-      <div style={{ marginBottom: '10px' }}>
-        <Todo text='Yrttilaatikon maalaus Tikkurilan Versolla' />
+      <div style={{ flexShrink: 0, marginBottom: '10px', marginRight: '10px' }}>
+        <Todo text='Kasvatusalustan maan muokkaus' />
       </div>
     </div>
   );
