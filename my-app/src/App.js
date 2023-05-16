3738cab6341a2fe88097dcd1796b9fb1c624cdc6
Sini Aaltonen <siniaaltonen@outlook.com>
2023-05-16 13:01:48 +0300

    Added taskes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,8 +3,9 @@ import Todo from '././components/Todo';
 function App() {
   return (
     <div>
-      <h1>Minun tehtävälistani:</h1>
+      <h1>Salajärven mökin askareet:</h1>
       <Todo text='Yrttien kasvatusalustan rakentaminen' />
+      <Todo text='Yrttilaatikon maalaus Tikkurilan Versolla' />
     </div>
   );
 }
