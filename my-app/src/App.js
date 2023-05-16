f2877936357061f2777629f1bfa38d484e39e7ac
Sini Aaltonen <siniaaltonen@outlook.com>
2023-05-16 14:03:20 +0300

    margin added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,8 +4,12 @@ function App() {
   return (
     <div>
       <h1>Salajärven mökin askareet:</h1>
-      <Todo text='Yrttien kasvatusalustan rakentaminen' />
-      <Todo text='Yrttilaatikon maalaus Tikkurilan Versolla' />
+      <div style={{ marginBottom: '10px' }}>
+        <Todo text='Yrttien kasvatusalustan rakentaminen' />
+      </div>
+      <div style={{ marginBottom: '10px' }}>
+        <Todo text='Yrttilaatikon maalaus Tikkurilan Versolla' />
+      </div>
     </div>
   );
 }
